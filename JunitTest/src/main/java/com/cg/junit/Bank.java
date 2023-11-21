cf619e5ebd9cb3290d8054acd2458dad45ba2d91
CheekatlaSnigdha <119494906+CheekatlaSnigdha@users.noreply.github.com>
2023-11-21 17:24:21 +0530

    Add files via upload

---
+++ JunitTest/src/main/java/com/cg/junit/Bank.java
@@ -0,0 +1,14 @@
+package com.cg.junit;
+
+public class Bank{
+	private int amount=10000;
+	public boolean withdraw(int amountToWithdraw) throws InsufficientFundsException {
+		if(amountToWithdraw>amount)
+		{
+			throw new InsufficientFundsException();
+		}
+		amount-=amountToWithdraw;
+		return true;
+	}
+}
+
---
+++ JunitTest/src/main/java/com/cg/junit/InsufficientFundsException.java
@@ -0,0 +1,8 @@
+package com.cg.junit;
+
+public class InsufficientFundsException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public InsufficientFundsException() {
+		System.out.println("Insufficient Funds");
+	}
+}
